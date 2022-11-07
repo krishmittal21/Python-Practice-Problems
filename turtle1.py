6d1194edba4c7194a6c964b133887db39ea2a98d
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2022-11-07 17:25:22 +0530

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,36 @@
+import turtle
+import math
+import random
+
+
+'''bob = turtle.Turtle()
+bob.forward(100)
+bob.left(45)
+bob.forward(100)
+bob.right(45)
+bob.color("blue")
+bob.speed(10)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.penup()
+bob.forward(150)
+bob.pendown()
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+'''
+
+bob= turtle.Turtle()
+
+
+
+turtle.done()
